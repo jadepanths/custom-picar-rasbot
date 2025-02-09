35b25232cd40bc3acd2a06bc810119006ab1c727
jadepanths <jadepanths@tamu.edu>
2025-02-09 18:59:50 +0700

    Fixed motor control by moving the function into the class

--- utils/motor_control.py
+++ utils/motor_control.py
@@ -25,29 +25,29 @@ class RaspbotCar:
         except:
             print("I2C error: Ctrl_Car")
 
-def run_forward(self):
-    """Move forward with an initial kick"""
-    self.Ctrl_Car(1, 80, 1, 80)  # Short burst of 80% power
-    time.sleep(0.2)  # Give it 0.2 seconds to start moving
-    self.Ctrl_Car(1, self.speed, 1, self.speed)  # Return to normal speed
+    def run_forward(self):
+        """Move forward with an initial kick"""
+        self.Ctrl_Car(1, 80, 1, 80)  # Short burst of 80% power
+        time.sleep(0.2)  # Give it 0.2 seconds to start moving
+        self.Ctrl_Car(1, self.speed, 1, self.speed)  # Return to normal speed
 
-def run_backward(self):
-    """Move backward with an initial kick"""
-    self.Ctrl_Car(0, 80, 0, 80)
-    time.sleep(0.2)
-    self.Ctrl_Car(0, self.speed, 0, self.speed)
+    def run_backward(self):
+        """Move backward with an initial kick"""
+        self.Ctrl_Car(0, 80, 0, 80)
+        time.sleep(0.2)
+        self.Ctrl_Car(0, self.speed, 0, self.speed)
 
-def turn_left(self):
-    """Turn left with an initial kick"""
-    self.Ctrl_Car(0, 80, 1, 80)
-    time.sleep(0.2)
-    self.Ctrl_Car(0, self.speed, 1, self.speed)
+    def turn_left(self):
+        """Turn left with an initial kick"""
+        self.Ctrl_Car(0, 80, 1, 80)
+        time.sleep(0.2)
+        self.Ctrl_Car(0, self.speed, 1, self.speed)
 
-def turn_right(self):
-    """Turn right with an initial kick"""
-    self.Ctrl_Car(1, 80, 0, 80)
-    time.sleep(0.2)
-    self.Ctrl_Car(1, self.speed, 0, self.speed)
+    def turn_right(self):
+        """Turn right with an initial kick"""
+        self.Ctrl_Car(1, 80, 0, 80)
+        time.sleep(0.2)
+        self.Ctrl_Car(1, self.speed, 0, self.speed)
 
     def stop(self):
         """Stop the car"""
