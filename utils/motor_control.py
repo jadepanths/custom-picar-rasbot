e25dca1bb75eabd856a2d6fc477b9c0ab66a87f5
jadepanths <jadepanths@tamu.edu>
2025-02-10 20:01:40 +0700

    adjust turning speed

--- utils/motor_control.py
+++ utils/motor_control.py
@@ -41,15 +41,15 @@ class RaspbotCar:
 
     def turn_left(self):
         """Turn left with a stronger initial kick"""
-        self.Ctrl_Car(0, 250, 1, 250)
-        time.sleep(0.15)
-        self.Ctrl_Car(0, 30, 1, 30)
+        self.Ctrl_Car(0, 200, 0, 200)  # Initial burst
+        time.sleep(0.1)
+        self.Ctrl_Car(0, self.speed, 1, self.speed)
 
     def turn_right(self):
         """Turn right with a stronger initial kick"""
-        self.Ctrl_Car(1, 250, 0, 250)
-        time.sleep(0.15)
-        self.Ctrl_Car(1, 30, 0, 30)
+        self.Ctrl_Car(0, 200, 0, 200)  # Initial burst
+        time.sleep(0.1)
+        self.Ctrl_Car(1, self.speed, 0, self.speed)
 
 
     def stop(self):
